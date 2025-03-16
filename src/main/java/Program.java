bb4cca74ceee3dec74328bc4219b817667f27378
gabeFrancisco <gabegit2@gmail.com>
2025-03-16 19:02:10 -0300

    implemented insertSeller method on Repository class

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -7,5 +7,9 @@ public static void main(String[] args) {
     repository.getAllDepartments();
     System.out.println("\n------------------");
     repository.getAllSellers();
+    System.out.println("\n------------------");
+    repository.insertSeller("Gabriel Francisco", "gabriel@mail.com", "29/03/1999", 3700.00, 1);
+    System.out.println("\n------------------");
+    repository.getAllSellers();
 
 }
--- src/main/java/Repository.java
+++ src/main/java/Repository.java
@@ -1,37 +1,39 @@
 import exceptions.DbException;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Repository {
-    private static Connection conn = null;
-    private static ResultSet rs = null;
+    private Connection conn = null;
+    private ResultSet rs = null;
+    private PreparedStatement ps = null;
 
-    public void getAllDepartments(){
-        try{
+    public void getAllDepartments() {
+        try {
             conn = DB.getConn();
             rs = conn.createStatement().executeQuery("select * from department");
 
-            while(rs.next()){
+            while (rs.next()) {
                 System.out.println(rs.getInt("Id") + ", " + rs.getString("name"));
             }
-        }
-        catch (SQLException e){
+        } catch (SQLException e) {
             throw new DbException(e.getMessage());
-        }
-        finally {
+        } finally {
             DB.closeResultSet(rs);
             DB.closeConn();
         }
     }
 
-    public void getAllSellers(){
-        try{
+    public void getAllSellers() {
+        try {
             conn = DB.getConn();
             rs = conn.createStatement().executeQuery("select * from seller");
 
-            while(rs.next()){
+            while (rs.next()) {
                 System.out.println(rs.getInt("Id")
                         + ", " + rs.getString("name")
                         + ", " + rs.getString("Email")
@@ -39,13 +41,38 @@ public class Repository {
                         + ", " + rs.getDouble("BaseSalary")
                         + ", " + rs.getInt("DepartmentId"));
             }
-        }
-        catch (SQLException e){
+        } catch (SQLException e) {
             throw new DbException(e.getMessage());
-        }
-        finally {
+        } finally {
             DB.closeResultSet(rs);
             DB.closeConn();
+        }
+    }
+
+    public void insertSeller(String name, String email, String birthDate, double baseSalary, int departmentId) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            conn = DB.getConn();
+            ps = conn.prepareStatement(
+                    "insert into seller "
+                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                            + "values "
+                            + "(?,?,?,?,?)");
+
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setDate(3, new java.sql.Date(sdf.parse(birthDate).getTime()));
+            ps.setDouble(4, baseSalary);
+            ps.setInt(5, departmentId);
+
+            int rows = ps.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rows);
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
         }
     }
 }
